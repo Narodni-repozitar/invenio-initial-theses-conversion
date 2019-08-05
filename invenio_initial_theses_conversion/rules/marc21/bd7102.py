add8576434ad4ba9563cdaa32860ba2f657b834f
semtex <Daniel.Kopecky@techlib.cz>
2019-08-05 14:09:23 +0200

    DegreeGrantor rules edited

--- invenio_initial_theses_conversion/rules/marc21/bd7102.py
+++ invenio_initial_theses_conversion/rules/marc21/bd7102.py
@@ -1,49 +1,62 @@
-from invenio_initial_theses_conversion.nusl_overdo import handled_values
+from flask_taxonomies.models import Taxonomy, TaxonomyTerm
+from invenio_initial_theses_conversion.nusl_overdo import handled_values, extra_argument
 from ..model import old_nusl
+from sqlalchemy.orm.exc import NoResultFound
+from invenio_initial_theses_conversion.scripts.link import link_self
 
 
 @old_nusl.over('degreeGrantor', '^7102_')
 @handled_values('a', '9', 'g', 'b')
-def degree_grantor(self, key, values):
+@extra_argument('provider', '^998__', single=True)
+def degree_grantor(self, key, values, provider):
     ret = [
         {
             "university": {
-                "name": [],
+                "$ref": "link",
+                "faculties": [
+                    {
+                        "$ref": "link",
+                        "departments": []
+                    }
+                ]
             }
         }
     ]
-    for item in values:
-        uni = ret[0]["university"]
-        uni["name"].append(
-            {
-                "name": item.get("a"),
-                "lang": item.get("9")
-            }
-        )
-        if item.get("g"):
-            if "faculties" not in uni:
-                uni["faculties"] = [
-                    {
-                        "name": []
-                    }
-                ]
-            faculty = ret[0]["university"]["faculties"][0]
-            faculty["name"].append(
-                {
-                    "name": item.get("g"),
-                    "lang": item.get("9")
-                }
-            )
-        if item.get("b"):
-            if "faculties" in uni:
-                if "departments" not in faculty:
-                    faculty["departments"] = []
-                department = faculty["departments"]
-                department.append(
-                    {
-                        "name": item.get("b"),
-                        "lang": item.get("9")
-                    }
-                )
 
-    return ret
+    universities = Taxonomy.get("universities", required=True)
+    provider_tax = Taxonomy.get("provider", required=True)
+    for item in values:
+        if item.get("9") == "cze":
+            uni_name = item.get("a")
+            university = find_uni_taxonomy(uni_name, provider, provider_tax, universities)
+            ret[0]["university"]["$ref"] = link_self(university)
+
+            if item.get("g"):
+                fac_name = item.get("g")
+                faculty = university.descendants.filter(
+                    TaxonomyTerm.extra_data[("name", 0, "name")].astext == fac_name).one()
+                ret[0]["university"]["faculties"][0]["$ref"] = link_self(faculty)
+            else:
+                del ret[0]["university"]["faculties"]
+
+            if item.get("b"):
+                ret[0]["university"]["faculties"][0]["departments"].append(item.get("b"))
+            else:
+                del ret[0]["university"]["faculties"][0]["departments"]
+
+        return ret
+
+
+def find_uni_taxonomy(uni_name, provider, provider_tax, universities):
+    university = universities.descendants.filter(
+        TaxonomyTerm.extra_data[("name", 0, "name")].astext == uni_name).all()
+    if len(university) == 0 and provider.get("a") is not None:
+        provider_term = provider_tax.descendants.filter(
+            TaxonomyTerm.slug == provider).one()
+        uni_name = provider_term.extra_data["name"][0]["name"]
+        find_uni_taxonomy(uni_name, provider, provider_tax, universities)
+    elif len(university) == 1:
+        university = university[0]
+    else:
+        raise NoResultFound
+    return university
