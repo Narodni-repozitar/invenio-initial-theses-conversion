51fb6c2c55896fa6238d68e91de94c5c80ac26cf
semtex <Daniel.Kopecky@techlib.cz>
2019-08-14 16:03:32 +0200

    subject

--- invenio_initial_theses_conversion/rules/marc21/bd650_653.py
+++ invenio_initial_theses_conversion/rules/marc21/bd650_653.py
@@ -1,5 +1,9 @@
+from flask_taxonomies.models import Taxonomy, TaxonomyTerm
 from invenio_initial_theses_conversion.nusl_overdo import append_results, list_value, handled_values
+from invenio_initial_theses_conversion.scripts.link import link_self
 from ..model import old_nusl
+from invenio_nusl.cli import create_slug
+from invenio_db import db
 
 
 @old_nusl.over("subject", '^650_7')
@@ -8,23 +12,68 @@ from ..model import old_nusl
 @handled_values('0', '2', 'a', 'j', '7')
 def subject(self, key, value):
     """Subject."""
+    subject_taxonomy = Taxonomy.get("subject", required=True)
+    extra_data = extra_data_dict(value)
+    if extra_data.get("taxonomy") is not None:
+        taxonomy = extra_data["taxonomy"]
+        if taxonomy.lower() == "mednas":
+            parent_term = subject_taxonomy.get_term("MEDNAS")
+            url = f"http://www.medvik.cz/link/{extra_data['id']}"
+            return taxonomy_ref(value.get("a"), parent_term, extra_data, subject_taxonomy, url=url, id=extra_data["id"])
+        if taxonomy.lower() == "czmesh":
+            parent_term = subject_taxonomy.get_term("CZMESH")
+            url = f"http://www.medvik.cz/link/{extra_data['id']}"
+            return taxonomy_ref(value.get("a"), parent_term, extra_data, subject_taxonomy, url=url, id=extra_data["id"])
+        if taxonomy.lower() == "psh":
+            parent_term = subject_taxonomy.get_term("PSH")
+
+
+def taxonomy_ref(keyword, parent_term, extra_data=None, taxonomy=None, url=None, id=None, lang=None):
+    subject = parent_term.descendants.filter(
+        TaxonomyTerm.slug == extra_data['id']).all()
+
+    if len(subject) == 0:
+        slug = add_to_taxonomy(keyword, parent_term, extra_data=extra_data, id=id, url=url, lang=lang)
+        subject = taxonomy.get_term(slug)
+
+        return {
+            "$ref": link_self(subject)
+        }
+    if len(subject) == 1:
+        return {
+            "$ref": link_self(subject[0])
+        }
+
+    if len(subject) > 0:
+        raise ValueError(f"In taxonomy database is duplicate record: {keyword}. Please check taxonomies")
+
+
+def extra_data_dict(value):
     if not value.get("a"):
         raise AttributeError("Subject name required (650_7a)")
     subject = {}
     if "a" in value:
-        subject["name"] = [
+        subject["title"] = [
             {
-                "name": value.get("a"),
+                "value": value.get("a"),
                 "lang": "cze"
             }
         ]
     if "j" in value:
-        subject["name"] = [
-            {
-                "name": value.get("j"),
-                "lang": "eng"
-            }
-        ]
+        if "title" not in subject:
+            subject["title"] = [
+                {
+                    "value": value.get("j"),
+                    "lang": "eng"
+                }
+            ]
+        else:
+            subject["title"].append(
+                {
+                    "value": value.get("j"),
+                    "lang": "eng"
+                }
+            )
     if "0" in value:
         if "7" in value:
             raise AttributeError("Id and URL must not be present in single subject (650_70, 650_77)")
@@ -35,62 +84,78 @@ def subject(self, key, value):
         subject["taxonomy"] = value.get("2")
     return subject
 
-    # """Subject."""
-    # if not value.get("a"):
-    #     raise AttributeError("Subject name required (650_7a)")
-    # subject = {}
-    # if "a" in value:
-    #     subject["name"] = value.get("a")
-    #     subject["lang"] = "cze"
-    # if "j" in value:
-    #     subject["name"] = value.get("j")
-    #     subject["lang"] = "eng"
-    # if "0" in value:
-    #     if "7" in value:
-    #         raise AttributeError("Id and URL must not be present in single subject (650_70, 650_77)")
-    #     subject["id"] = value.get("0")
-    # if "7" in value:
-    #     subject["id"] = value.get("7")
-    # if "2" in value:
-    #     subject["taxonomy"] = value.get("2")
-    # return subject
-
 
 @old_nusl.over("subject", '^653')
 @append_results
 @list_value
 @handled_values('a')
-def keyword(self, key, value):
+def subject(self, key, value):
     """Subject."""
     if key == '653__':
-        return {
-            "name": [
-                {
-                    "name": value.get("a"),
-                    "lang": "cze"
-                }
-            ]
-        }
+        keyword = value.get("a")
+        subjects = Taxonomy.get("subject", required=True)
+        parent_term = subjects.get_term("keyword")
+        return taxonomy_ref(keyword, parent_term, lang=keyword)
+        # subject = subjects.descendants.filter(
+        #     TaxonomyTerm.extra_data["title"].contains([{"value": keyword}])).all()
+        # if len(subject) == 0:
+        #     slug = add_to_taxonomy(keyword, subjects, lang="cze")
+        #     subject = subjects.get_term(slug)
+        #
+        #     return {
+        #         "$ref": link_self(subject)
+        #     }
+        # if len(subject) == 1:
+        #     return {
+        #         "$ref": link_self(subject[0])
+        #     }
+        #
+        # if len(subject) > 0:
+        #     raise ValueError(f"In taxonomy database is duplicate record: {keyword}. Please check taxonomies")
 
     if key == '6530_':
-        return {
-            "name": [
+        keyword = value.get("a")
+        subjects = Taxonomy.get("subject", required=True)
+        # subject = subjects.descendants.filter(
+        #     TaxonomyTerm.extra_data["title"].contains([{"value": keyword}])).all()
+        # if len(subject) == 0:
+        #     slug = add_to_taxonomy(keyword, subjects, lang="eng")
+        #     subject = subjects.get_term(slug)
+        #
+        #     return {
+        #         "$ref": link_self(subject)
+        #     }
+        # if len(subject) == 1:
+        #     return {
+        #         "$ref": link_self(subject[0])
+        #     }
+        #
+        # if len(subject) > 0:
+        #     raise ValueError(f"In taxonomy database is duplicate record: {keyword}. Please check taxonomies")
+
+
+def add_to_taxonomy(keyword, taxonomy_term=None, extra_data=None, lang=None, id=None, url=None):
+    if id is None:
+        slug = create_slug(keyword)
+    else:
+        slug = id
+    if extra_data is None:
+        extra_data = {
+            "title": [
                 {
-                    "name": value.get("a"),
-                    "lang": "eng"
+                    "value": keyword,
+                    "lang": lang
                 }
             ]
         }
-
-    # """Subject."""
-    # if key == '653__':
-    #     return {
-    #         "name": value.get("a"),
-    #         "lang": "cze"
-    #     }
-    #
-    # if key == '6530_':
-    #     return {
-    #         "name": value.get("a"),
-    #         "lang": "eng"
-    #     }
+    if id is not None:
+        extra_data["id"] = id
+    if url is not None:
+        extra_data["url"] = url
+    subject = taxonomy_term.create_term(
+        slug=slug,
+        extra_data=extra_data
+    )
+    db.session.add(subject)
+    db.session.commit()
+    return slug
--- invenio_initial_theses_conversion/scripts/link.py
+++ invenio_initial_theses_conversion/scripts/link.py
@@ -1,17 +1,20 @@
 import os
 
+reverse_path = []
+
 
 def link_self(tax):
-    def path_self(tax, path):
-        path.append(tax.slug)
-        if tax.level == 1:
-            return path[::-1]
-        if getattr(tax, "taxonomy", None) is not None:
-            return path_self(tax.taxonomy, path)
-        return path[::-1]
-
     SERVER_NAME = os.environ.get('SERVER_NAME')
     base = f"https://{SERVER_NAME}/api/taxonomies"
-    path = []
-    path = [base] + path_self(tax, path)
+    taxonomy = tax.taxonomy.slug
+    tree_path = tax.tree_path
+    path = [base, taxonomy + tree_path]
     return "/".join(path)
+
+
+# def recursive_parent(tax):
+#     if tax.parent is not None:
+#         reverse_path.append(tax.slug)
+#         recursive_parent(tax)
+#     else:
+#         return reverse_path
