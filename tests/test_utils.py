1e8ef326712a2a8e37ccffcac7da98aca4df840b
Daniel Kopecky <kopecky.d@gmail.com>
2019-11-05 15:23:36 +0100

    split_keywords tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,13 +12,37 @@ def test_split_keywords_1():
             "a": "internacionalismus|neologismus|lexikální sémantika|pragmatika|kultura|politika|zpravodajství|deník"})),
     )
     res = split_keywords(keywords)
-    pass
+    assert res == (
+        GroupableOrderedDict(OrderedDict({'a': 'mladší školní věk'})),
+        GroupableOrderedDict(OrderedDict({'a': 'slovo'})),
+        GroupableOrderedDict(OrderedDict({'a': 'internacionalismus'})),
+        GroupableOrderedDict(OrderedDict({'a': 'neologismus'})),
+        GroupableOrderedDict(OrderedDict({'a': 'lexikální sémantika'})),
+        GroupableOrderedDict(OrderedDict({'a': 'pragmatika'})),
+        GroupableOrderedDict(OrderedDict({'a': 'kultura'})),
+        GroupableOrderedDict(OrderedDict({'a': 'politika'})),
+        GroupableOrderedDict(OrderedDict({'a': 'zpravodajství'})),
+        GroupableOrderedDict(OrderedDict({'a': 'deník'}))
+    )
 
 
 def test_split_keywords_2():
     keywords = (
         GroupableOrderedDict(OrderedDict({"a": "mladší školní věk - slovo"})),
         GroupableOrderedDict(OrderedDict({
-            "a": "internacionalismus|neologismus|lexikální sémantika|pragmatika|kultura|politika|zpravodajství|deník"})),
+            "a": "internacionalismus - neologismus - lexikální sémantika - pragmatika - kultura - politika - "
+                 "zpravodajství - deník"})),
     )
-    split_keywords(keywords)
+    res = split_keywords(keywords)
+    assert res == (
+        GroupableOrderedDict(OrderedDict({'a': 'mladší školní věk'})),
+        GroupableOrderedDict(OrderedDict({'a': 'slovo'})),
+        GroupableOrderedDict(OrderedDict({'a': 'internacionalismus'})),
+        GroupableOrderedDict(OrderedDict({'a': 'neologismus'})),
+        GroupableOrderedDict(OrderedDict({'a': 'lexikální sémantika'})),
+        GroupableOrderedDict(OrderedDict({'a': 'pragmatika'})),
+        GroupableOrderedDict(OrderedDict({'a': 'kultura'})),
+        GroupableOrderedDict(OrderedDict({'a': 'politika'})),
+        GroupableOrderedDict(OrderedDict({'a': 'zpravodajství'})),
+        GroupableOrderedDict(OrderedDict({'a': 'deník'}))
+    )
