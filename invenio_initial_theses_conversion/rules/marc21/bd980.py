d30bf1599a2f27ecfe99d46ea35902a16e4a1955
Daniel Kopecky <kopecky.d@gmail.com>
2020-03-20 16:58:40 +0100

    Update doctype rule

--- invenio_initial_theses_conversion/rules/marc21/bd980.py
+++ invenio_initial_theses_conversion/rules/marc21/bd980.py
@@ -1,4 +1,6 @@
 from flask_taxonomies.models import Taxonomy, TaxonomyTerm
+
+from flask_taxonomies_es.proxies import current_flask_taxonomies_es
 from invenio_initial_theses_conversion.nusl_overdo import single_value, handled_values, list_value
 from invenio_initial_theses_conversion.scripts.link import link_self
 from ..model import old_nusl
@@ -8,6 +10,11 @@ from ..model import old_nusl
 @handled_values('a')
 def doctype(self, key, values):
     doc_type = values[0].get('a')
+    es_result = current_flask_taxonomies_es.get("doctype", doc_type)
+    if es_result:
+        return {
+            "$ref": es_result["links"]["self"]
+        }
     doctype_tax = Taxonomy.get("doctype", required=True)
     doctype = doctype_tax.descendants.filter(
         TaxonomyTerm.slug == doc_type).one()
---
+++ tests/rules/test_bd980.py
@@ -0,0 +1,13 @@
+from urllib.parse import urlparse
+
+from invenio_initial_theses_conversion.rules.marc21.bd980 import doctype
+
+
+def test_doctype_1(app, db, overdo_instance):
+    value = {
+        'a': 'bakalarske_prace'
+    }
+    res = doctype(overdo_instance, "key", [value])
+    assert isinstance(res, dict)
+    url = urlparse(res["$ref"])
+    assert url.path == '/api/taxonomies/doctype/nusl/vskp/bakalarske_prace/'
