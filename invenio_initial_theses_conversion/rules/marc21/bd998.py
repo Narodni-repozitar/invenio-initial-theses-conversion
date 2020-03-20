c86091dbb4bd1821f1c42da3c9c736ea0e0092e2
Daniel Kopecky <kopecky.d@gmail.com>
2020-03-20 16:59:33 +0100

    Updated provider rule

--- invenio_initial_theses_conversion/rules/marc21/bd998.py
+++ invenio_initial_theses_conversion/rules/marc21/bd998.py
@@ -1,6 +1,8 @@
 from flask import url_for
 
 from flask_taxonomies.models import Taxonomy, TaxonomyTerm
+
+from flask_taxonomies_es.proxies import current_flask_taxonomies_es
 from invenio_initial_theses_conversion.nusl_overdo import single_value, handled_values
 from invenio_initial_theses_conversion.scripts.link import link_self
 from ..model import old_nusl
@@ -10,14 +12,17 @@ from ..model import old_nusl
 @handled_values('a')
 @single_value
 def provider(self, key, value):
-    data = value.get('a')
+    slug = value.get('a')
+    provider = current_flask_taxonomies_es.get('provider', slug)
+    if provider:
+        return {
+            "$ref": provider["links"]["self"]
+        }
     tax = Taxonomy.get("provider", required=True)
     provider = tax.descendants.filter(
-        TaxonomyTerm.slug == data).one()  # viz:
+        TaxonomyTerm.slug == slug).one()  # viz:
     # https://stackoverflow.com/questions/29974143/python-sqlalchemy-and-postgres-how-to-query-a
     # -json-element
-    result = {
+    return {
         "$ref": link_self(tax.slug, provider)
     }
-
-    return result
---
+++ tests/rules/test_bd998.py
@@ -0,0 +1,14 @@
+from urllib.parse import urlparse
+
+from invenio_initial_theses_conversion.rules.marc21.bd998 import provider
+
+
+def test_provider_1(app, db, overdo_instance):
+    value = {
+        'a': 'jihoceska_univerzita_v_ceskych_budejovicich'
+    }
+    res = provider(overdo_instance, "key", [value])
+    assert isinstance(res, dict)
+    url = urlparse(res["$ref"])
+    assert url.path == '/api/taxonomies/provider/edu/public_uni' \
+                       '/jihoceska_univerzita_v_ceskych_budejovicich/'
