5cdb1741b86010b65ce4e074c5cff97e473e744c
Daniel Kopecky <kopecky.d@gmail.com>
2020-04-06 13:48:58 +0200

    Update studyfields

--- invenio_initial_theses_conversion/rules/marc21/bd656.py
+++ invenio_initial_theses_conversion/rules/marc21/bd656.py
@@ -1,31 +1,27 @@
+import json
+
 from elasticsearch_dsl import Q
 from flask_taxonomies.models import Taxonomy
-from flask_taxonomies.utils import find_in_json_contains
 
 from flask_taxonomies_es.proxies import current_flask_taxonomies_es
-from invenio_initial_theses_conversion.nusl_overdo import single_value, merge_results, \
-    extra_argument
+from invenio_initial_theses_conversion.nusl_overdo import single_value, merge_results
 from ..model import old_nusl
-from ..utils import db_search, jsonify_fields, get_ref_es
+from ..utils import db_search, get_ref_es
 
 
 @old_nusl.over("studyProgramme", '^656_7')
 @merge_results
 @single_value
-@extra_argument('grantor', '^7102', single=True)
-@extra_argument('doc_type', '^980', single=True)
-def study_programme_field(self, key, value, grantor, doc_type):
+def study_programme_field(self, key, value):
     """Study programme."""
-    doc_type = doc_type.get("a")
-    grantor = grantor.get("a").lower()
-    slug = value.get("a")
+    source = value.get("a")
     tax = Taxonomy.get("studyfields", required=True)
-    if "/" not in slug:
-        return studyfield_ref(slug.strip(), tax, grantor, doc_type)
+    if "/" not in source:
+        return studyfield_ref(source.strip(), tax)
     else:
-        programme, field = slug.split("/", maxsplit=1)
+        programme, field = source.split("/", maxsplit=1)
         field = field.strip()
-        return studyfield_ref(field, tax, grantor, doc_type)
+        return studyfield_ref(field, tax)
 
 
 def es_search_title(study, tax):
@@ -33,72 +29,29 @@ def es_search_title(study, tax):
     return current_flask_taxonomies_es.search(query)
 
 
-def es_search_aliases(study, tax):
-    query = Q("term", taxonomy__keyword=tax) & Q("term", aliases__keyword=study)
-    return current_flask_taxonomies_es.search(query)
-
-
-def studyfield_ref(study_title, tax, grantor: str, doc_type: str):
+def studyfield_ref(study_title, tax):
     # https://docs.sqlalchemy.org/en/13/dialects/postgresql.html#sqlalchemy.dialects.postgresql.JSON
     # https://github.com/sqlalchemy/sqlalchemy/issues/3859  # issuecomment-441935478
+    if study_title is None:
+        return
     fields = es_search_title(study_title, tax.slug)
     if len(fields) == 0:
         fields = db_search(study_title, tax, json_address=("title", 0, "value"))
-    if len(fields) == 0:
-        fields = aliases(tax, study_title)
-    if len(fields) == 0:
-        return []
-    if len(fields) > 1:
-        fields = filter(fields, doc_type, grantor)
-
-    return {
-        "studyField": [get_ref_es(field) for field in fields],
-    }
-
-
-def aliases(tax, study):
-    fields = es_search_aliases(study, tax.slug)
-    if len(fields) == 0:
-        fields = db_search(study, tax, json_address="aliases")
-    if len(fields) == 0:
-        fields = find_in_json_contains(study, tax, tree_address="aliases").all()
-        fields = jsonify_fields(fields)
-    return fields
-
-
-def filter(fields, doc_type, grantor):
-    fields = doc_filter(fields, doc_type)
-    fields = grantor_filter(fields, grantor)
-    return fields
-
-
-def doc_filter(fields, doc_type):
-    if doc_type is not None:
-        if doc_type == "rigorozni_prace":
-            doc_type = "disertacni_prace"
-        degree_dict = {
-            "Bakalářský": "bakalarske_prace",
-            "Magisterský": "diplomove_prace",
-            "Navazující magisterský": "diplomove_prace",
-            "Doktorský": "disertacni_prace"
+    if len(fields) > 0:
+        fields_filtered = [field for field in fields if field["slug"].startswith("P")]
+        if len(fields_filtered) > 0:
+            fields = fields_filtered
+        return {
+            "studyField": [get_ref_es(field) for field in fields],
         }
-        new_fields = [field for field in fields if
-                      degree_dict.get(field.get("programme_type")) == doc_type]
-        if len(new_fields) == 0:
-            new_fields = [field for field in fields if "programme_type" not in field.keys()]
-        return new_fields
-    return fields
-
-
-def grantor_filter(fields, grantor):
-    if grantor is not None:
-        matched_fields = [field for field in fields if
-                          field.get("grantor") is not None and
-                          field["grantor"][0][
-                              "university"].lower() == grantor.lower()]
-        if len(matched_fields) == 0:
-            return fields
-        else:
-            return matched_fields
     else:
-        return fields
+        return studyfield_ref(aliases(study_title), tax)
+
+
+def aliases(field):
+    from pathlib import Path
+    package_dir = Path(__file__).parents[2]
+    path = package_dir / "data" / "aliases.json"
+    with open(str(path), "r") as f:
+        alias_dict = json.load(f)
+    return alias_dict.get(field)
--- tests/rules/test_bd656.py
+++ tests/rules/test_bd656.py
@@ -1,15 +1,10 @@
-from pprint import pprint
 from urllib.parse import urlparse
 
-import pytest
 from flask_taxonomies.models import Taxonomy
 from invenio_search import current_search_client
 
-from .fixtures import grantor_filter_fix, doc_filter_fix
-
 from invenio_initial_theses_conversion.rules.marc21.bd656 import study_programme_field, \
-    doc_filter, \
-    es_search_title, grantor_filter, es_search_aliases, aliases
+    es_search_title, aliases
 from invenio_initial_theses_conversion.rules.utils import db_search
 
 
@@ -18,17 +13,10 @@ def test_study_programme_field_1(app, db, overdo_instance):
         'a': 'Psychologie/Arteterapie',
         '2': 'AKVO'
     }
-    doc_type = {
-        'a': 'bakalarske_prace'
-    }
-    grantor = {
-        'a': 'JIHOČESKÁ UNIVERZITA V ČESKÝCH BUDĚJOVICÍCH',
-        '9': 'cze'
-    }
-    results = study_programme_field(overdo_instance, "key", [value], grantor, doc_type)
+    results = study_programme_field(overdo_instance, "key", [value])
     assert isinstance(results, dict)
     url = urlparse(results["studyField"][0]["$ref"])
-    assert url.path == '/api/taxonomies/studyfields/7701R012'
+    assert url.path == '/api/taxonomies/studyfields/O_arteterapie/'
 
 
 def test_study_programme_field_2(app, db, overdo_instance):
@@ -36,17 +24,10 @@ def test_study_programme_field_2(app, db, overdo_instance):
         'a': 'Psychologie',
         '2': 'AKVO'
     }
-    doc_type = {
-        'a': 'bakalarske_prace'
-    }
-    grantor = {
-        'a': 'JIHOČESKÁ UNIVERZITA V ČESKÝCH BUDĚJOVICÍCH',
-        '9': 'cze'
-    }
-    results = study_programme_field(overdo_instance, "key", [value], grantor, doc_type)
+    results = study_programme_field(overdo_instance, "key", [value])
     assert isinstance(results, dict)
     url = urlparse(results["studyField"][0]["$ref"])
-    assert url.path == '/api/taxonomies/studyfields/7701R005'
+    assert url.path == '/api/taxonomies/studyfields/P_psychologie/'
 
 
 def test_study_programme_field_3(app, db, overdo_instance):
@@ -54,17 +35,11 @@ def test_study_programme_field_3(app, db, overdo_instance):
         'a': 'Učitelství pro střední školy/ČJ-L/SŠ',
         '2': 'AKVO'
     }
-    doc_type = {
-        'a': 'bakalarske_prace'
-    }
-    grantor = {
-        'a': 'JIHOČESKÁ UNIVERZITA V ČESKÝCH BUDĚJOVICÍCH',
-        '9': 'cze'
-    }
-    results = study_programme_field(overdo_instance, "key", [value], grantor, doc_type)
+    results = study_programme_field(overdo_instance, "key", [value])
     assert isinstance(results, dict)
     url = urlparse(results["studyField"][0]["$ref"])
-    assert url.path == '/api/taxonomies/studyfields/7504T039'
+    assert url.path == '/api/taxonomies/studyfields/O_ucitelstvi-ceskeho-jazyka-a-literatury-pro' \
+                       '-stredni-skoly/'
 
 
 def test_study_programme_field_4(app, db, overdo_instance):
@@ -72,14 +47,8 @@ def test_study_programme_field_4(app, db, overdo_instance):
         'a': 'blbost',
         '2': 'AKVO'
     }
-    doc_type = {
-        'a': 'bakalarske_prace'
-    }
-    grantor = {
-        'a': 'JIHOČESKÁ UNIVERZITA V ČESKÝCH BUDĚJOVICÍCH',
-        '9': 'cze'
-    }
-    results = study_programme_field(overdo_instance, "key", [value], grantor, doc_type)
+
+    results = study_programme_field(overdo_instance, "key", [value])
     assert results is None
 
 
@@ -90,17 +59,10 @@ def test_study_programme_field_5(app, db, overdo_instance):
             'a': 'Psychologie/Arteterapie',
             '2': 'AKVO'
         }
-        doc_type = {
-            'a': 'bakalarske_prace'
-        }
-        grantor = {
-            'a': 'JIHOČESKÁ UNIVERZITA V ČESKÝCH BUDĚJOVICÍCH',
-            '9': 'cze'
-        }
-        results = study_programme_field(overdo_instance, "key", [value], grantor, doc_type)
+        results = study_programme_field(overdo_instance, "key", [value])
         assert isinstance(results, dict)
         url = urlparse(results["studyField"][0]["$ref"])
-        assert url.path == '/api/taxonomies/studyfields/7701R012'
+        assert url.path == '/api/taxonomies/studyfields/O_arteterapie/'
     finally:
         with app.app_context():
             current_search_client.indices.delete(
@@ -109,22 +71,7 @@ def test_study_programme_field_5(app, db, overdo_instance):
 
 def test_es_search_title(app, db):
     results = es_search_title("Filozofie", "studyfields")
-    assert len(results) == 8
-
-
-def test_es_search_aliases(app, db):
-    results = es_search_aliases("ČJ-L/SŠ", "studyfields")
-    assert len(results) == 1
-
-
-def test_doc_filter(doc_filter_fix):
-    res = doc_filter(doc_filter_fix, 'bakalarske_prace')
-    assert len(res) == 1
-
-
-def test_grantor_filter(grantor_filter_fix):
-    res = grantor_filter(grantor_filter_fix, 'Jihočeská univerzita v Českých Budějovicích')
-    assert len(res) == 2
+    assert len(results) == 2
 
 
 def test_db_search(app, db):
@@ -138,20 +85,5 @@ def test_db_search(app, db):
 
 
 def test_aliases(app, db):
-    try:
-        app.config.update(TAXONOMY_ELASTICSEARCH_INDEX="test_taxonomies_es")
-        tax = Taxonomy.get("studyfields", required=True)
-        fields = aliases(tax, "ČJ-L/SŠ")
-        assert isinstance(fields, list)
-        for field in fields:
-            assert "slug" in field.keys()
-            assert "title" in field.keys()
-            assert "links" in field.keys()
-    finally:
-        with app.app_context():
-            current_search_client.indices.delete(
-                index="test_taxonomies_es")
-
-
-def test_650(app, db, overdo_instance):
-    print('ahoj')
+    ret = aliases('Zemědělství - Prvovýroba')
+    assert ret == 'Zemědělství'
