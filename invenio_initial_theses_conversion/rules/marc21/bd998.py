61abae8c3c30550d54eb717b463b6b15f8bec603
Daniel Kopecky <kopecky.d@gmail.com>
2020-04-14 18:30:42 +0200

    Update provider

--- invenio_initial_theses_conversion/rules/marc21/bd998.py
+++ invenio_initial_theses_conversion/rules/marc21/bd998.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import url_for
 
 from flask_taxonomies.models import Taxonomy, TaxonomyTerm
@@ -13,11 +15,14 @@ from ..utils import get_ref_es
 @handled_values('a')
 @single_value
 def provider(self, key, value):
-    slug = value.get('a')
-    provider = current_flask_taxonomies_es.get('provider', slug)
+    nusl_slug = value.get("a")
+    slug = get_slug(nusl_slug).strip()
+    if not slug:
+        return
+    provider = current_flask_taxonomies_es.get('institutions', slug)
     if provider:
         return get_ref_es(provider)
-    tax = Taxonomy.get("provider", required=True)
+    tax = Taxonomy.get("institutions", required=True)
     provider = tax.descendants.filter(
         TaxonomyTerm.slug == slug).one()  # viz:
     # https://stackoverflow.com/questions/29974143/python-sqlalchemy-and-postgres-how-to-query-a
@@ -25,3 +30,12 @@ def provider(self, key, value):
     return {
         "$ref": link_self(tax.slug, provider)
     }
+
+
+def get_slug(nusl_slug):
+    from pathlib import Path
+    package_dir = Path(__file__).parents[2]
+    path = package_dir / "data" / "providers.json"
+    with open(str(path), "r") as f:
+        nusl_mapping = json.load(f)
+    return nusl_mapping.get(nusl_slug)
